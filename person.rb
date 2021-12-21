ed32150c96a3be74b26c932c21cbb0445046ab35
ahmedadel56 <ahmdadl563@gmail.com>
2021-12-21 23:15:17 +0200

    Decorate a class

--- person.rb
+++ person.rb
@@ -1,15 +1,22 @@
+require_relative 'corrector'
+
 class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @correct_instance = Corrector.new
   end
   attr_reader :id
   attr_accessor :name, :age
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def validate_name
+    @name = @correct_instance.correct_name(@name)
   end
 
   private
